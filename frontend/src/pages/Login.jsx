5793dcddc647d8cd13c2fce3e9466a5e5da57c32
vaishnavi640 <vaishnavikalluri@gmail.com>
2025-02-19 10:08:30 +0530

    Css changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Login from '../components/auth/Login';
 
 const LoginPage = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-green-400 to-teal-500">
+    <div className="h-screen flex justify-center items-center bg-blue-900 ">
       <div className="w-full sm:max-w-md p-4">
         <Login />
       </div>
